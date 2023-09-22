b805c8b9c38fb89fff74199cbb9e85e57c3eb2a8
BT_2N2_14 <sample@bths.edu>
2023-09-22 13:38:00 -0400

    initial commit

---
+++ src/RectangleRunner.java
@@ -0,0 +1,22 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+        // creating one instance/object from the Rectangle class
+        Rectangle rect1 = new Rectangle(5, 6);
+        rect1.printArea();  // calling a method on the object
+
+        // creating ANOTHER instance/object from the Rectangle class
+        Rectangle rect2 = new Rectangle(10, 8);
+        rect2.printArea();  // calling a method on the object
+
+        Rectangle rect3 = new Rectangle((int)6.5,(int)10.5);
+        rect3.printArea();
+
+        rect1.printPerimeter();
+        rect2.printPerimeter();
+        rect3.printPerimeter();
+
+    }
+}
+
+
